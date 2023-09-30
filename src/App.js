b9aa4d17d26b4d9e3a927f82fa30f568175f30be
AsmaaElawady <101433174+AsmaaElawady@users.noreply.github.com>
2023-10-01 01:27:44 +0300

    Shopping Category &  Single Product Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Product from './pages/Product';
 import Home from './pages/Home';
+import ProductList from './pages/ProductList';
+
 
 function App() {
   return (
     <div>
-      <Home />
+      <Product/>
     </div>
   );
 }
---
+++ src/pages/ProductList.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+import styled from "styled-components";
+import  Announcement  from "../components/Announcements"
+import NewsLetter from '../components/NewsLetter';
+import Footer from '../components/Footer';
+import Products from '../components/Products';
+
+const Container = styled.div``;
+
+const Title = styled.h1`
+  margin: 20px;
+`;
+
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Filter = styled.div`
+  margin: 20px;
+`;
+
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+  margin-right: 20px;
+`;
+
+const Select = styled.select`
+  padding: 10px;
+  margin-right: 20px;
+`;
+const Option = styled.option``;
+
+export default function ProductList() {
+  return (
+    <Container>
+        <NavBar/>
+        <Announcement/>
+        <Title>Dresses</Title>
+        <FilterContainer>
+            <Filter>
+                <FilterText>Filter Products:</FilterText>
+            <Select>
+                <Option disabled selected>
+                    color
+                </Option>
+                <Option>White</Option>
+                <Option>Black</Option>
+                <Option>Red</Option>
+                <Option>Blue</Option>
+                <Option>Yellow</Option>
+                <Option>Green</Option>
+            </Select>
+            <Select>
+                <Option disabled selected>
+                    Size
+                </Option>
+                <Option>XS</Option>
+                <Option>S</Option>
+                <Option>M</Option>
+                <Option>L</Option>
+                <Option>XL</Option>
+            </Select>
+            </Filter>
+            <Filter>
+                <FilterText>Sort Products:</FilterText>
+                <Select>
+                <Option  selected> Newset</Option>
+                <Option>Price (asc)</Option>
+                <Option>Price (desc)</Option>
+            </Select>
+            </Filter>
+        </FilterContainer>
+        <Products/>
+        <NewsLetter/>
+        <Footer />
+    </Container>
+  )
+}
